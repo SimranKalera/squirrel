ed9d40ce264c6246f076a41afac5b54770b461a9
Simran Kalera <simran21496@gmail.com>
2019-12-02 06:14:52 +0000

    Modifying import management command

--- project/tracker/management/commands/import_squirrel_data.py
+++ project/tracker/management/commands/import_squirrel_data.py
@@ -14,34 +14,34 @@ class Command(BaseCommand):
         if not os.path.exists(file_path):
             raise CommandError("%s doesnt exist"%file_path)
         
-        data = pd.read_csv(file_path, delimiter=',')
+        data = pd.read_csv(file_path, delimiter=',', error_bad_lines=False)
         squirrels = [
                 Squirrel(
-                    latitude = data.ix[row]['Y'],
-                    longitude = data.ix[row]['X'],
-                    unique_id = data.ix[row]['Unique Squirrel ID'],
-                    shift = data.ix[row]['Shift'],
-                    date = data.ix[row]['Date'],
-                    age = data.ix[row]['Age'],
-                    primary_fur_color = data.ix[row]['Primary Fur Color'],
-                    location = data.ix[row]['Location'],
-                    specific_location = data.ix[row]['Specific Location'],
-                    quaas = data.ix[row]['Quaas'],
-                    moans = data.ix[row]['Moans'],
-                    tail_flags = data.ix[row]['Tail flags'],
-                    tail_twitches = data.ix[row]['Tail twitches'],
-                    approaches = data.ix[row]['Approaches'],
-                    indifferent = data.ix[row]['Indifferent'],
-                    runs_from = data.ix[row]['Runs from'],
-                    running = data.ix[row]['Running'],
-                    chasing = data.ix[row]['Chasing'],
-                    climbing = data.ix[row]['Climbing'],
-                    eating = data.ix[row]['Eating'],
-                    foraging = data.ix[row]['Foraging'],
-                    other_activities = data.ix[row]['Other Activites'],
-                    kuks = data.ix[row]['Kuks'],
+                    latitude = row[1][1],
+                    longitude = row[1][0],
+                    unique_id = row[1][2],
+                    shift = row[1][4],
+                    date = row[1][5],
+                    age = row[1][7],
+                    primary_fur_color = row[1][8],
+                    location = row[1][12],
+                    specific_location = row[1][14],
+                    quaas = row[1][22],
+                    moans = row[1][23],
+                    tail_flags = row[1][24],
+                    tail_twitches = row[1][25],
+                    approaches = row[1][26],
+                    indifferent = row[1][27],
+                    runs_from = row[1][28],
+                    running = row[1][15],
+                    chasing = row[1][16],
+                    climbing = row[1][17],
+                    eating = row[1][18],
+                    foraging = row[1][19],
+                    other_activities = row[1][20],
+                    kuks = row[1][21],
                     )
-               for row in data
+               for row in data.iterrows()
                ]
         Squirrel.objects.bulk_create(squirrels)
             
