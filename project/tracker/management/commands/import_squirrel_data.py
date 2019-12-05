1edfb4678404e9d25c8feb9bf4bf3f79021e5147
Simran Kalera <simran21496@gmail.com>
2019-12-05 03:57:13 +0000

    recreated db,imported csv using management command

--- project/tracker/management/commands/import_squirrel_data.py
+++ project/tracker/management/commands/import_squirrel_data.py
@@ -1,47 +1,50 @@
-from django.core.management.base import BaseCommand
-import os, csv, sys
-import pandas as pd
+import csv
+from django.db import IntegrityError
+from django.core.management import BaseCommand
 from tracker.models import Squirrel
+import argparse
 
 class Command(BaseCommand):
-    help = 'Import data from 2018 census file'
-
-    def add_arguments(self, parser):
-        parser.add_argument('file_path', type=str, help='Indicates the file path of the squirrel data file')
+    help = 'Load a csv file into the database'
+    
+    def add_arguments(self,parser):
+        parser.add_argument('path', type=str)
 
     def handle(self, *args, **kwargs):
-        file_path = sys.argv[0]
-        if not os.path.exists(file_path):
-            raise CommandError("%s doesnt exist"%file_path)
-        
-        data = pd.read_csv(file_path, delimiter=',', error_bad_lines=False)
-        squirrels = [
-                Squirrel(
-                    latitude = tuple(pd.DataFrame(row[1]).iloc[1]),
-                    longitude = tuple(pd.DataFrame(row[1]).iloc[0]),
-                    unique_id = tuple(pd.DataFrame(row[1]).iloc[2]),
-                    shift = tuple(pd.DataFrame(row[1]).iloc[4]),
-                    date = tuple(pd.DataFrame(row[1]).iloc[5]),
-                    age = tuple(pd.DataFrame(row[1]).iloc[7]),
-                    primary_fur_color = tuple(pd.DataFrame(row[1]).iloc[8]),
-                    location = tuple(pd.DataFrame(row[1]).iloc[12]),
-                    specific_location = tuple(pd.DataFrame(row[1]).iloc[14]),
-                    quaas = tuple(pd.DataFrame(row[1]).iloc[22]),
-                    moans = tuple(pd.DataFrame(row[1]).iloc[23]),
-                    tail_flags = tuple(pd.DataFrame(row[1]).iloc[24]),
-                    tail_twitches = tuple(pd.DataFrame(row[1]).iloc[25]),
-                    aeproaches = tuple(pd.DataFrame(row[1]).iloc[26]),
-                    indifferent = tuple(pd.DataFrame(row[1]).iloc[27]),
-                    runs_from = tuple(pd.DataFrame(row[1]).iloc[28]),
-                    running = tuple(pd.DataFrame(row[1]).iloc[15]),
-                    chasing = tuple(pd.DataFrame(row[1]).iloc[16]),
-                    climbing = tuple(pd.DataFrame(row[1]).iloc[17]),
-                    eating = tuple(pd.DataFrame(row[1]).iloc[18]),
-                    foraging = tuple(pd.DataFrame(row[1]).iloc[19]),
-                    other_activities = tuple(pd.DataFrame(row[1]).iloc[20]),
-                    kuks = tuple(pd.DataFrame(row[1]).iloc[21]),
+        print('Deleting all sightings..')
+        Squirrel.objects.all().delete()
+        print('Importing..');
+        path = kwargs['path']
+        with open(path, 'rt') as f:
+            reader = csv.reader(f, dialect='excel')
+            next(reader,None)
+            for row in reader:
+                try: 
+                    squirrel = Squirrel.objects.create(
+                        longitude=row[0],
+                        latitude=row[1],
+                        unique_id=row[2],
+                        shift=row[4],
+                        date=row[5],
+                        age=row[7],
+                        primary_fur_color=row[8],
+                        location=row[12],
+                        specific_location=row[14],
+                        running=(row[15]=='true'),
+                        chasing=(row[16]=='true'),
+                        climbing=(row[17]=='true'),
+                        eating=(row[18]=='true'),
+                        foraging=(row[19]=='true'),
+                        other_activities=row[20],
+                        kuks=(row[21]=='true'),
+                        quaas=(row[22]=='true'),
+                        moans=(row[23]=='true'),
+                        tail_flags=(row[24]=='true'),
+                        tail_twitches=(row[25]=='true'),
+                        approaches=(row[26]=='true'),
+                        indifferent=(row[27]=='true'),
+                        runs_from=(row[28] == 'true'),
                     )
-               for row in data.iterrows()
-               ]
-        Squirrel.objects.bulk_create(squirrels)
-            
+                    squirrel.save()
+                except IntegrityError as e:
+                    print('Skipping', row[2], 'due to integrity error')
