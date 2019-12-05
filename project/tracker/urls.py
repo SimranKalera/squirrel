40cd93e364b4f9381d5558c43b359bce711c7ae1
Simran Kalera <simran21496@gmail.com>
2019-12-05 07:32:39 +0000

    pulling from master

--- project/tracker/urls.py
+++ project/tracker/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('sightings/', views.all_squirrels),
+        path('sightings/', views.sightings),
+        path('map/',views.map),
+        path('sightings/<str:unique_id>/', views.update),
 ]
