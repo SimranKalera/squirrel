a99e0bf1054e5e266f21db02c6a68b1a221da3c7
Amrita Dutta <amrita.dutta1497@gmail.com>
2019-12-05 07:27:26 +0000

    added map, sightings and update views

--- project/tracker/views.py
+++ project/tracker/views.py
@@ -3,8 +3,46 @@ from django.http import HttpResponse
 
 from .models import Squirrel
 
-def all_squirrels(request):
-    squirrels = Squirrel.objects.get(request)
-    return Httpresponse("Hi")
+
+def map(request):
+    sightings= Squirrel.objects.all()[:100]
+    context={
+            'sightings':sightings,
+        }
+    return render(request, 'tracker/map.html',context)
+
+
+def sightings(request):
+    squirrels= Squirrel.objects.all()
+    context = {
+            'squirrels': squirrels,
+            }
+    return render(request, 'tracker/sightings.html', context)
+
+def update(request, unique_id):
+    squirrel= Squirrel.objects.get(id= unique_id)
+    if request.method == 'POST':
+        form = SquirrelForm (request.POST, instance = squirrel)
+
+        if form.is_valid():
+            form.save()
+            return redirect(f'/tracker/{unique_id}')
+
+    else:
+        form = SquirrelForm()
+    context = {
+            'form': form,
+            }
+    return render(request, 'tracker/update.html', context)
+
+
+
+
+
+
+
+
+
+
 
 # Create your views here.
