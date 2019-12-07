39daf0ee71f19bcf0a3b6d94550b1f5aba7ed698
Simran Kalera <simran21496@gmail.com>
2019-12-07 06:10:38 +0000

    Updated the urls and added forms

--- project/tracker/urls.py
+++ project/tracker/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('sightings/', views.sightings),
-        path('map/',views.map),
+        path('sightings', views.sightings),
+        path('map',views.map),
         path('sightings/<str:unique_id>/', views.update),
-]
+        path('sightings/add',views.create),
+        path('sightings/stats',views.view_stats),
+        
+      ]
