6da5817150b656ba9dd34194664f570df0afbbe1
Amrita Dutta <amrita.dutta1497@gmail.com>
2019-12-07 05:31:58 +0000

    Adding html for views and updating urls

--- project/tracker/urls.py
+++ project/tracker/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
         path('sightings/', views.sightings),
         path('map/',views.map),
         path('sightings/<str:unique_id>/', views.update),
-]
+        path('sightings/add',views.create),
+        path('sightings/stats',views.view_stats),
+        
+      ]
